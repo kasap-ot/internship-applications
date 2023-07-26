3ff04c4d163e3eb16939d7f2b877be32df81575c
kasap-ot <63372251+kasap-ot@users.noreply.github.com>
2023-07-26 03:15:04 +0200

    Remove unnecessary code

--- resources/views/components/button-link.blade.php
+++
@@ -1,9 +0,0 @@
-@props(['route', 'text', 'color'])
-
-@php
-    $classes = 'bg-'.$color.'-500 hover:bg-'.$color.'-700 text-white font-bold py-2 px-4 rounded'
-@endphp
-
-<a href="{{ $route }}" {{ $attributes->merge(['class' => $classes]) }}>
-    {{ $text }}
-</a>
