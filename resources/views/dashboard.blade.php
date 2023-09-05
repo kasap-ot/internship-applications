08f6ebdd1212ddf9591cf3d8e22374cd92368b37
kasap-ot <63372251+kasap-ot@users.noreply.github.com>
2023-09-05 17:33:17 +0200

    Add main buttons to dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,6 +12,62 @@
                     {{ __("You're logged in!") }}
                 </div>
             </div>
+            
+            @can('is-admin')
+                <div class=" my-4 flex justify-between space-x-6">
+                    <a href="{{route('user-requests')}}" class="w-1/3">
+                        <x-secondary-button class="flex justify-center w-full h-60">
+                            User requests
+                        </x-primary-button>
+                    </a>
+                    <a href="{{route('verified-users')}}" class="w-1/3">
+                        <x-secondary-button class="flex justify-center w-full h-60">
+                            Verified users
+                        </x-primary-button>                    
+                    </a>
+                    <a href="{{route('profile.edit')}}" class="w-1/3">
+                        <x-secondary-button class="flex justify-center w-full h-60">
+                            My profile
+                        </x-primary-button>
+                    </a>
+                </div>
+            @endcan
+            
+            @can('is-company')
+                <div class=" my-4 flex justify-between space-x-6">
+                    <a href="{{route('offers.index')}}" class="w-1/2">
+                        <x-secondary-button class="flex justify-center w-full h-60">
+                            My offers
+                        </x-primary-button>                    
+                    </a>
+                    <a href="{{route('profile.edit')}}" class="w-1/2">
+                        <x-secondary-button class="flex justify-center w-full h-60">
+                            My profile
+                        </x-primary-button>
+                    </a>
+                </div>
+            @endcan
+
+            @can('is-student')
+            <div class=" my-4 flex justify-between space-x-6">
+                <a href="{{route('offers.index')}}" class="w-1/3">
+                    <x-secondary-button class="flex justify-center w-full h-60">
+                        Offers
+                    </x-primary-button>
+                </a>
+                <a href="{{route('applications')}}" class="w-1/3">
+                    <x-secondary-button class="flex justify-center w-full h-60">
+                        My applications
+                    </x-primary-button>                    
+                </a>
+                <a href="{{route('profile.edit')}}" class="w-1/3">
+                    <x-secondary-button class="flex justify-center w-full h-60">
+                        My profile
+                    </x-primary-button>
+                </a>
+            </div>
+            @endcan
+
         </div>
     </div>
 </x-app-layout>
